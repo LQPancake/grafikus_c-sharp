a2102b2949914b993411189515b1057daa014f5b
Pancake <baligajozsef@gmail.com>
2026-02-09 12:37:13 +0100

    -

--- kekturaForm/ClassLibraryKektura/Class1.cs
+++ kekturaForm/ClassLibraryKektura/Class1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +16,44 @@ namespace ClassLibraryKektura
         public int emelkedes { get; private set; }
         public int lejtes { get; private set; }
         public bool pecseteloHely { get; private set; }
+        public static int kezdoMagassag { get; private set; }
+        public static List<kektura> kekturaLista = new List<kektura>();
+
+        public kektura(string sor)
+        {
+            string[] db = sor.Split(';');
+            this.kiinduloPont = db[0];
+            this.vegPont = db[1];
+            this.szakaszHosszKm = double.Parse(db[2]);
+            this.emelkedes = int.Parse(db[3]);
+            this.lejtes = int.Parse(db[4]);
+            if (db[5] == "i")
+            {
+                this.pecseteloHely = true;
+            }
+            else
+            {
+                this.pecseteloHely = false;
+            }
+        }
+        public static List<kektura> FileBeolvasas(string filenev)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(filenev);
+                kezdoMagassag = int.Parse(sr.ReadLine());
+                while (!sr.EndOfStream)
+                {
+                    var obj = new kektura(sr.ReadLine());
+                    kekturaLista.Add(obj);
+                }
+                sr.Close();
+            }
+            catch
+            {
+                throw new Exception("Hiba a fájl megnyitása során");
+            }
+        return kekturaLista;
+        }
     }
 }
--- kekturaForm/kekturaForm/Form1.cs
+++ kekturaForm/kekturaForm/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibraryKektura;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using kekturaForm;
 
 namespace kekturaForm
 {
     public partial class Form1 : Form
     {
+        private static List<kektura> lathatoLista = new List<kektura>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                kektura.FileBeolvasas("kektura.csv");
+                teljesListaMasolat();
+                listaMegjelenites();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba a fájl beolvasásakor!", ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listBoxUtvonalLista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void teljesListaMasolat()
+        {
+            foreach (var obj in kektura.kekturaLista)
+            {
+                lathatoLista.Add(obj);
+            }
+        }
+        private void listaMegjelenites()
+        {
+            listBoxUtvonalLista.Items.Clear();
+            foreach (var obj in lathatoLista)
+            {
+                listBoxUtvonalLista.Items.Add(obj.kiinduloPont);
+            }
+        }
+
+        private void labelDarabszam_Click(object sender, EventArgs e)
+        {
+            foreach (var obj in lathatoLista)
+            {
+                labelDarabszam.Text = lathatoLista.Count + "Db";
+            }
+        }
+
+        private void buttonAdatok_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = listBoxUtvonalLista.SelectedItem;
+            }
         }
     }
 }
